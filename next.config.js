b411e3cc46773090c1064847d078f3c252360072
lakshegde1988 <lakshhegde1988@gmail.com>
2024-11-06 10:21:22 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  // Add this if you're using a custom path for components
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname),
+    };
+    return config;
+  },
 }
 
 module.exports = nextConfig
