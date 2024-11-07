a4a37955a86522aa6f8e0dadd581e09f63075a46
lakshegde1988 <lakshhegde1988@gmail.com>
2024-11-07 11:55:14 +0530

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,10 @@
+// app/layout.tsx
+import "../styles/globals.css"; 
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
