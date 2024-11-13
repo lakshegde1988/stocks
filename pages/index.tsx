c46490a7a3aee6870fa59031a1ab3b6525f4c3cc
lakshegde1988 <lakshhegde1988@gmail.com>
2024-11-13 18:21:22 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -403,7 +403,7 @@ export default function Component() {
             </div>
           )}
 
-          <Card className="bg-slate-800/50">
+          <Card className="bg-slate-800/50 bg-slate-800/50">
             <CardContent className="p-0 sm:p-2">
               {loading ? (
                 <div className="h-[600px] flex flex-col items-center justify-center">
