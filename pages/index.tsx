2def9bdb98a4d7cda0a28f2d2d345756a9286f16
lakshegde1988 <lakshhegde1988@gmail.com>
2024-11-15 19:13:54 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -116,7 +116,7 @@ export default function StockChart() {
   const searchRef = useRef<HTMLDivElement>(null);
 
   const getChartHeight = useCallback(() => {
-    return window.innerWidth < 640 ? 550 : window.innerWidth < 1024 ? 350 : 650;
+    return window.innerWidth < 640 ? 450 : window.innerWidth < 1024 ? 350 : 650;
   }, []);
 
   useEffect(() => {
