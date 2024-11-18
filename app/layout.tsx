c466cec60912eb89d3a6b429d2f22d8e65098f05
lakshegde1988 <lakshhegde1988@gmail.com>
2024-11-18 18:54:03 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,29 @@
 // app/layout.tsx
+
+import { ThemeProvider } from 'next-themes'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
 import "../styles/globals.css"; 
 
-export default function RootLayout({ children }) {
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'DotChart',
+  description: 'Stock chart application',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
-  );
+  )
 }
