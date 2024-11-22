4007eb3298681dc74cada537b4b2a1413063c5de
lakshegde1988 <lakshhegde1988@gmail.com>
2024-11-22 19:11:14 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-use client';
+'use client';
 
 import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { createChart, ColorType, IChartApi, ISeriesApi, BarData, HistogramData } from 'lightweight-charts';
