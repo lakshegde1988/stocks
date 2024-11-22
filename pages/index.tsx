5b20e3188c0fe97be98eb19180e9f0c251a3fbd9
lakshegde1988 <lakshhegde1988@gmail.com>
2024-11-22 19:38:34 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -405,20 +405,7 @@ export default function StockChart() {
             )}
           </div>
 
-          {/* Theme Toggle Button */}
-          <Button
-            variant="ghost"
-            size="icon"
-            onClick={toggleTheme}
-            className="h-8 w-8 p-0"
-          >
-            {theme === 'dark' ? (
-              <Sun className="h-4 w-4" />
-            ) : (
-              <Moon className="h-4 w-4" />
-            )}
-            <span className="sr-only">Toggle theme</span>
-          </Button>
+         
 
           {/* Full Screen Button (visible only on mobile) */}
           <Button
