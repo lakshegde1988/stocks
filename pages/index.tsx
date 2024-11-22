541a79152f55695db677237e1ac38d2158ccd936
lakshegde1988 <lakshhegde1988@gmail.com>
2024-11-22 18:52:20 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
-import { WatchlistModal } from './components/WatchlistModal';
+import { WatchlistModal } from '../components/WatchlistModal';
 
 import nifty50Data from '../public/nifty50.json';
 import niftyNext50Data from '../public/niftynext50.json';
